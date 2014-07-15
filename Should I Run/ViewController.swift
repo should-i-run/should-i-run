f3ce5c8c134f9f04f8306f2a46e5003bab0b0858
hungrycat <rgoldfinger@gmail.com>
2014-07-15 12:13:19 -0700

    Initial Commit

---
+++ Should I Run/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Should I Run
+//
+//  Created by Roger Goldfinger on 7/15/14.
+//  Copyright (c) 2014 Should I Run. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Should I RunTests/Should_I_RunTests.swift
@@ -0,0 +1,36 @@
+//
+//  Should_I_RunTests.swift
+//  Should I RunTests
+//
+//  Created by Roger Goldfinger on 7/15/14.
+//  Copyright (c) 2014 Should I Run. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Should_I_RunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

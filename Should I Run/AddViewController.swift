6108d2f6550bbdf7f212c5ff441e96426d6ca3cc
hungrycat <rgoldfinger@gmail.com>
2014-07-15 16:25:07 -0700

    center the map on the user

--- Should I Run/AddViewController.swift
+++ Should I Run/AddViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 import MapKit
 
-class AddViewController: UIViewController, MKMapViewDelegate {
+class AddViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
 
     @IBOutlet var textField : UITextField
 
 
     @IBOutlet var saveBarButton: UIBarButtonItem
+    
+    
+    @IBOutlet var mapView: MKMapView
+    
+    var locationManager = CLLocationManager()
 
 
     var place:Place? = nil
@@ -40,13 +45,46 @@ class AddViewController: UIViewController, MKMapViewDelegate {
                         userDefaults.synchronize()
         
     }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//    
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //check required for iOS 8.
+        //we see if the location manager has this request method. 
+        //If so we are on 8 and need to request auth
+        if self.locationManager.respondsToSelector(Selector("requestAlwaysAuthorization")) {
+                    self.locationManager.requestWhenInUseAuthorization()
+        }
+        
+        
+        self.locationManager.delegate = self
+        
+        //We don't need to be very accurate here, since we're just centering the map
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        self.locationManager.startUpdatingLocation()
+
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+
+        //convert the user's location to a 2d coordinate
+        let loc2d: CLLocationCoordinate2D =  locationManager.location.coordinate
+        
+        //create a 'region' with the user's location as the center, and set the map to that region
+        let reg = MKCoordinateRegionMakeWithDistance(loc2d, 20000, 20000)
+        self.mapView.setRegion(reg, animated: false)
+
+        
+    }
+    
+    //    - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+    //    {
+    //    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    //    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+    //    }
+    
 
 //
 //    override func didReceiveMemoryWarning() {

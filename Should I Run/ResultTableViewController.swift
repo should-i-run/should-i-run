6faf3f2d45562814c87d554224bfad4a2503acc3
LM <lmay01@gmail.com>
2014-07-16 17:46:45 -0700

    implement transition from BART api XML into native data structure

--- Should I Run/ResultTableViewController.swift
+++ Should I Run/ResultTableViewController.swift
@@ -21,6 +21,53 @@ class ResultTableViewController: UITableViewController {
         
         self.destinationCell.textLabel.text = "You're going to " + self.locationName!
 
+        // Fetch information for the BART api and convert the returned XML into a dictionary
+        var url = NSURL(string: "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=cols&key=ZELI-U2UY-IBKQ-DT35")
+        var data = NSMutableData.dataWithContentsOfURL(url, options: NSDataReadingOptions.DataReadingUncached, error: nil)
+        let html = NSString(data: data, encoding: NSUTF8StringEncoding)
+        var parsed: NSDictionary = XMLReader.dictionaryForXMLString(html, error: nil)
+
+        // Trim off unneeded data inside the dictionary
+        var stations: NSDictionary = parsed.objectForKey("root").objectForKey("station") as NSDictionary
+        
+        // Create an array of tuples to store our destination stations (termini) and their estimated arrival time to our closest BART  station
+        var allResults: [(String, Int)] = []
+        
+        // Iterate over our stations
+        for item in stations {
+            if (item.key as NSString == "etd") {
+                // We need to check if one result or multiple results exist. If we have multiple results, we'll do a loop. Otherwise, we can access terminus abbreviation and estimated time directly
+                if (item.value["abbreviation"]) {
+                    var myTuple: (String, Int)
+                    var abbr = item.value["abbreviation"] as NSDictionary
+                    for estimateItem in item.value["estimate"] as [AnyObject] {
+                        var estimateMin = estimateItem["minutes"] as NSDictionary
+                        
+                        // Create the terminus and estimated arrival tuple and push into our results
+                        var myTuple: (String, Int) = (abbr["text"] as String, estimateMin["text"].integerValue)
+                        allResults += myTuple
+                    }
+
+                } else {
+                    for stationItem in item.value as [AnyObject] {
+                        var abbr = stationItem["abbreviation"] as NSDictionary
+                        for estimateItem in stationItem["estimate"] as [AnyObject] {
+                            var estimateMin = estimateItem["minutes"] as NSDictionary
+
+                            // Create the terminus and estimated arrival tuple and push into our results
+                            var myTuple: (String, Int) = (abbr["text"] as String, estimateMin["text"].integerValue)
+                            allResults += myTuple
+                        }
+                    }
+                }
+            }
+        }
+
+        // Sort the tuple array of termini and estimated arrival in ascending order
+
+        allResults.sort{$0.1 < $1.1}
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -28,6 +75,7 @@ class ResultTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

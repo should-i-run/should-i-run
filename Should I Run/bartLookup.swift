59dcb8aa3e152cb2180a5a94f1aa0ccf1e41a008
neillobo <neillobo@outlook.com>
2014-07-17 12:26:45 -0700

    Google Directions data with Bart station codes, multiple routes remaining

---
+++ Should I Run/bartLookup.swift
@@ -0,0 +1,55 @@
+//
+//  bartLookup.swift
+//  Should I Run
+//
+//  Created by Neil Lobo on 7/16/14.
+//  Copyright (c) 2014 Should I Run. All rights reserved.
+//
+
+import Foundation
+
+var bartLookup : [String : String] = ["12th St. Oakland City Center" : "12th",
+    "16th St. Mission":"16th",
+    "19th St. Oakland":"19th",
+    "24th St. Mission":"24th",
+    "Ashby":"ashb",
+    "Balboa Park":"balb",
+    "Bay Fair":"bayf",
+    "Castro Valley":"cast",
+    "Civic Center/UN Plaza":"civc",
+    "Coliseum/Oakland Airport":"cols",
+    "Colma":"colm",
+    "Concord":"conc",
+    "Daly City":"daly",
+    "Downtown Berkeley":"dbrk",
+    "Dublin/Pleasanton":"dubl",
+    "El Cerrito del Norte":"deln",
+    "El Cerrito Plaza":"plza",
+    "Embarcadero":"embr",
+    "Fremont":"frmt",
+    "Fruitvale":"ftvl",
+    "Glen Park":"glen",
+    "Hayward":"hayw",
+    "Lafayette":"lafy",
+    "Lake Merritt":"lake",
+    "MacArthur":"mcar",
+    "Millbrae":"mlbr",
+    "Montgomery St.":"mont",
+    "North Berkeley":"nbrk",
+    "North Concord/Martinez":"ncon",
+    "Orinda":"orin",
+    "Pittsburg/Bay Point":"pitt",
+    "Pleasant Hill":"phil",
+    "Powell St.":"powl",
+    "Richmond":"rich",
+    "Rockridge":"rock",
+    "San Bruno":"sbrn",
+    "San Francisco Int'l Airport":"sfia",
+    "San Leandro":"sanl",
+    "South Hayward":"shay",
+    "South San Francisco":"ssan",
+    "Union City":"ucty",
+    "Walnut Creek":"wcrk",
+    "West Dublin":"wdub",
+    "West Oakland":"woak"
+]

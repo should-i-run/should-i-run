bd553be80601a140ec0b9c8e2c31dcc69f06454d
Roger Goldfinger <rgoldfinger@gmail.com>
2015-10-13 20:38:21 -0700

    setup cocoapods

---
+++ Should_I_Run/Should_I_RunTests/Should_I_RunTests.swift
@@ -0,0 +1,36 @@
+//
+//  Should_I_RunTests.swift
+//  Should_I_RunTests
+//
+//  Created by Roger Goldfinger on 11/30/15.
+//  Copyright © 2015 Should_I_Run. All rights reserved.
+//
+
+import XCTest
+@testable import Should_I_Run
+
+class Should_I_RunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

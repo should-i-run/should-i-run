5b9fafbe294ce882e314bf2614cf4c575d51fc11
Roger Goldfinger <rgoldfinger@gmail.com>
2016-04-09 19:06:58 -0700

    install react-native

---
+++ Should_I_Run/ReactComponents/index.ios.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import React, {
+  Text,
+  View
+} from 'react-native';
+
+var styles = React.StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red'
+  }
+});
+
+class SimpleApp extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is a simple application.</Text>
+      </View>
+    )
+  }
+}
+
+React.AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
